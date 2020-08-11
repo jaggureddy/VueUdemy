923eb122a76d54d788e95c5ba8376e8817a4f3f9
JagguReddy <Jagadeesh6010@gmail.com>
2020-08-10 21:16:22 -0400

    Vue -2

--- vue.js
+++ vue.js
@@ -1,13 +1,20 @@
 new Vue({
   el: '#app',
   data: {
-    title: 'Hello Vue'
+    title: 'Hello Vue',
+    link : "http://google.com/",
+    finishedLink: '<a href="http://google.com">Google</a>'
   },
   methods: {
-
     changeTitle: function(event) {
       this.title = event.target.value;
+    },
+    sayHello : function(){
+      return "Hello Vue!"
+    },
+    testVOnce : function(){
+      this.title = "Hello v once";
+      return this.title;
     }
-
   }
 });
